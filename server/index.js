43b73eb0fe37329d7b3fb995a3944d784f8342d2
iamrithu <ritimahesh29@gmail.com>
2022-06-28 16:48:23 +0530

    better

--- server/index.js
+++ server/index.js
@@ -103,7 +103,7 @@ export async function createServer(
       fontColor: req.body.fontColor,
       fontFamily: req.body.fontFamily,
       fontSize: req.body.fontSize,
-      shopDetails: test_session.shop,
+      shopDetails: { shopd: { connect: { name: test_session.shop } } },
       isActive: "true",
     };
 
