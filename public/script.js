fba21dba9b7bac0f56b40984cf77a16a57f205e1
iamrithu <ritimahesh29@gmail.com>
2022-06-30 14:17:53 +0530

    added

---
+++ public/script.js
@@ -0,0 +1,26 @@
+const announcementBar = document.getElementById(
+  "shopify-section-announcement-bar"
+);
+announcementBar.style.marginBottom = "10px";
+announcementBar.innerHTML = `<div
+      style="
+        height: 35px;
+        width: 100%;
+        background: url();
+        background: {{background}};
+        margin: 0px;
+        margin-bottom:10px;
+        display: flex;
+        position:{{position}};
+        align-items: center;
+        justify-content: center;
+        color: {{color}};
+        font-size: {{font-size}};
+        font-family: {{font-family}}
+          ;
+      "
+    >
+      {{content}}
+    </div>`;
+
+//DB=mongodb+srv://shopify:shopify@cluster0.gdtlt.mongodb.net/Shopify?retryWrites=true&w=majority
