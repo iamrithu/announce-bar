924c0008a166477d24748641cbd3cf5bc4c36d4e
iamrithu <ritimahesh29@gmail.com>
2022-06-30 13:52:17 +0530

    added

--- server/index.js
+++ server/index.js
@@ -90,23 +90,26 @@ export async function createServer(
         isActive: "true",
       },
     });
-    console.log(data);
 
-    // var data = {
-    //   uuid: uuid(),
-    //   name: req.body.name,
-    //   content: req.body.shipBar,
-    //   background: req.body.background,
-    //   position: req.body.position,
-    //   fontColor: req.body.fontColor,
-    //   fontFamily: req.body.fontFamily,
-    //   fontSize: req.body.fontSize,
-    //   shop: test_session.shop,
-    //   isActive: "true",
-    // };
-    // await prisma.shipbars.create({
-    //   data: data,
-    // });
+    await prisma.shipbars.update({
+      where: { uuid: data[0].uuid },
+      data: { isActive: "false" },
+    });
+    var data = {
+      uuid: uuid(),
+      name: req.body.name,
+      content: req.body.shipBar,
+      background: req.body.background,
+      position: req.body.position,
+      fontColor: req.body.fontColor,
+      fontFamily: req.body.fontFamily,
+      fontSize: req.body.fontSize,
+      shop: test_session.shop,
+      isActive: "true",
+    };
+    await prisma.shipbars.create({
+      data: data,
+    });
   });
 
   app.get("/products-count", verifyRequest(app), async (req, res) => {
