482c7dc7cc8242b96f48eb2c752ea682caabce5f
iamrithu <ritimahesh29@gmail.com>
2022-07-11 11:02:47 +0530

    added instruction

--- src/screen/HomePage.jsx
+++ src/screen/HomePage.jsx
@@ -1,5 +1,13 @@
 import { Page } from "@shopify/polaris";
 import { Table } from "./components/Table.jsx";
+import styled from "styled-components";
+
+const container = styled.div`
+  width: 90%;
+  height: 400px;
+  z-index: 100;
+  border-radius: 10px;
+`;
 
 export function HomePage() {
   return (
@@ -8,17 +16,7 @@ export function HomePage() {
         <h2>Announcement</h2>
         <button>click</button>
       </div>
-      <div
-        style={{
-          position: "absolute",
-          width: "100%",
-          height: "200px",
-          background: "grey",
-          zIndex: "100",
-        }}
-      >
-        hello
-      </div>
+      <container>hello</container>
       <Table />
     </Page>
   );
