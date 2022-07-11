6708338c23a830f41513904191f42056ff3efa94
iamrithu <ritimahesh29@gmail.com>
2022-07-11 10:48:12 +0530

    added instruction

--- src/screen/HomePage.jsx
+++ src/screen/HomePage.jsx
@@ -3,8 +3,14 @@ import { Table } from "./components/Table.jsx";
 
 export function HomePage() {
   return (
-    <Page title="Announcement" fullWidth>
-      <p>demo</p>
+    <Page fullWidth>
+      <div style={{ display: "flex" }}>
+        <h2>Announcement</h2>
+        <button>click</button>
+      </div>
+      <div
+        style={{ position: "absolute", width: "100%", height: "200px" }}
+      ></div>
       <Table />
     </Page>
   );
