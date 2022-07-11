8f483277387eb03b7f9651cb75928ddc452ee607
iamrithu <ritimahesh29@gmail.com>
2022-07-11 11:34:56 +0530

    added instruction

--- src/screen/HomePage.jsx
+++ src/screen/HomePage.jsx
@@ -9,7 +9,6 @@ const Container = styled.div`
   height: 400px;
   z-index: 100;
   border-radius: 10px;
-  display:  ${(props) => (props.active ? "block" : "none")};,
   position: absolute;
   top: 10%;
   left: 5%;
@@ -31,22 +30,27 @@ export function HomePage() {
         <h2>Announcement</h2>
         <button onClick={open}>click</button>
       </div>
-      <Container active={showInstruction}>
-        <div>
-          <h2>Instruction</h2>
-        </div>
-        <div>
-          <ol>
-            <li>Add a Condition and select "Based on order price"</li>
-            <li>Enter a minimum order price</li>
-            <li>Once you complete it, click "Done"</li>
-            <li>
-              You can create a bar by clicking the "Create New Bar" button below
-            </li>
-          </ol>
-        </div>
-      </Container>
-      <Table />
+
+      {showInstruction ? (
+        <Container>
+          <div>
+            <h2>Instruction</h2>
+          </div>
+          <div>
+            <ol>
+              <li>Add a Condition and select "Based on order price"</li>
+              <li>Enter a minimum order price</li>
+              <li>Once you complete it, click "Done"</li>
+              <li>
+                You can create a bar by clicking the "Create New Bar" button
+                below
+              </li>
+            </ol>
+          </div>
+        </Container>
+      ) : (
+        <Table />
+      )}
     </Page>
   );
 }
