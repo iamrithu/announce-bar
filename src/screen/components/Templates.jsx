f07997e3bbd55f833506f7b4aeaa331f41f21f14
iamrithu <ritimahesh29@gmail.com>
2022-07-13 14:55:13 +0530

    added shiping goal

--- src/screen/components/Templates.jsx
+++ src/screen/components/Templates.jsx
@@ -322,7 +322,7 @@ const Templates = ({ getTemplate, closeTemplate }) => {
                   type="color"
                   onChange={(e) => set_background_color(e.target.value)}
                   autoComplete="off"
-                  style={{ width: "200px", height: "40px", marginLeft: "10px" }}
+                  style={{ width: "200px", height: "40px", marginLeft: "8px" }}
                 />
               </div>
               <div
@@ -348,7 +348,7 @@ const Templates = ({ getTemplate, closeTemplate }) => {
                   type="color"
                   onChange={(e) => set_font_color(e.target.value)}
                   autoComplete="off"
-                  style={{ width: "200px", height: "40px", marginLeft: "50px" }}
+                  style={{ width: "200px", height: "40px", marginLeft: "55px" }}
                 />
               </div>
               <div
